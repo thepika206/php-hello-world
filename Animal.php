90a31bbb79d6844a3ab3bc2ea155fec467980e2f
pcheung <pika206@gmail.com>
2024-02-05 22:03:48 -0600

    dog subclass

--- Animal.php
+++ Animal.php
@@ -2,24 +2,57 @@
     class Animal {
         public $name = '';
         public $type = '';
-        
+        public $age = 0;
+
+        function __construct($name, $type, $age) {
+            $this->name = $name;
+            $this->type = $type;
+            $this->age = $age;
+
+        }
+        //animal name
         function set_name($name) {
             $this->name = $name;
         }
-        function set_type($type) {
-            $this->type = $type;
-        }
-
         function get_name() {
             return $this->name;
         }
-
-        function speak(){
-            echo "$this->name says hi";
+        //animal type
+        function set_type($type) {
+            $this->type = $type;
+        }
+        function get_type() {
+            return $this->type;
+        }
+        //animal age
+        function set_age($age) {
+            $this->age = $age;
+        }
+        function get_age() {
+            return $this->age;
         }
 
         function introduce() {
             echo "$this->name the $this->type";
+        }
+    }
+
+    class Dog extends Animal {
+        public $trainingLevel = 0;
+
+        function __construct($name, $type, $age) {
+            parent::__construct($name, $type, $age);
+            echo "Created instance of Dog, name: $this->name, type: $this->type, age: $this->age.";
+        }
+
+        function train() {
+            $this->trainingLevel += 1;
+            echo "$this->name now has a training level of: $this->trainingLevel ";
+            echo "<br>";
+        }
+
+        function getTrainingLevel(){
+            return $this->trainingLevel;
         }
     }
 
--- index.php
+++ index.php
@@ -1,13 +1,37 @@
+<?php 
+        include 'Animal.php';
+       
+        
+?>
+
 <html>
 <body>
     <h1>Hello World PHP!</h1>
-    <h2>My pet</h2>
-    <?php 
-        include 'Animal.php';
-        $dog = new Animal;
-        $dog->set_name('Cheddar');
-        $dog->set_type('Dog');
-        $dog->introduce();
-    ?>
+    <h2>Summary</h2>
+    <ul>
+        <li>Get a php file running in a browser</li>
+        <li>Practice creating an Animal Class and Dog sub-class</li>
+    </ul>
+    <h2>Create a new Dog, subclass of Animal</h2>
+    <p>
+        <?php
+            $dog = new Dog("Cheddar", "dog", 1);
+        ?>
+    </p>
+    <h2>How does var dump look</h2>
+    <p>
+        var dump of $dog:
+        <?php
+            var_dump($dog);
+        ?>
+    </p>
+    <h2>Use the train() method on Dog</h2>
+    <p>
+        <?php 
+            $dog->train();
+            $dog->train();
+        ?>
+    </p>
+    
 </body>
 </html>
